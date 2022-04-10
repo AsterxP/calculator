039456751a82ad8e533c6a128c01b9f8da78d620
Aster <bmarch87@gmail.com>
2022-04-10 15:23:27 +1000

    fixed calculations, changed div's to buttons

--- js/app.js
+++ js/app.js
@@ -1,42 +1,90 @@
-// display
+// get HTML elements
 const display = document.querySelector(`.display`);
+const numbers = document.querySelectorAll(`.number`);
+const operator = document.querySelectorAll(`.operator`);
+const decimal = document.querySelector(`.decimal`);
+const clear = document.querySelector(`.clear`);
+
+//
+let total = 0;
+let a = '';
+let b = '';
+let currentOperator = '';
+let lastOperator = '';
+let haveDot = false;
 
 //click
-const buttons = document.querySelectorAll(`.btn`)
-buttons.forEach((button) => {
+numbers.forEach((button) => {
   button.addEventListener('click', () => {
-    console.log(button.TextContent);
-    display.textContent = button.textContent;
+    if (button.textContent === '.' && !haveDot) {
+      haveDot = true;
+    } else if (button.textContent === '.' && haveDot) {
+      return;
+    }
+    b += button.textContent;
+    display.textContent = b;
   });
+});
+
+
+operator.forEach((button) => {
+  button.addEventListener('click', () => {
+    if(a && b && lastOperator){
+      currentOperator = button.textContent;
+      operate(lastOperator, a, b);
+    } else {
+      currentOperator = button.textContent
+      total = b;
+    }  
+    clearVar();
+    console.log(total)  
+  });
+});
+
+function clearVar() {
+  display.textContent = total;
+  a = total;
+  b = '';
+  lastOperator = currentOperator;
+  haveDot = false;
+}
+
+clear.addEventListener('click', () => {
+  display.textContent = "";
+  a = '';
+  b = '';
+  total = 0;
+  haveDot = false;
 });
 
 // calculations
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+const divide = function(a, b) {
+  if (b == 0) {
+    return `ERROR`;
+  } else
+    return a / b;
+}
 
-function operate(a, b, c) {
-  if(!Number.isInteger(a) || !Number.isInteger(c)) {
-    return 'ERROR'
-  } else {
-    answer = 0;
-    switch (b) {
-      case `+`:
-        answer = add(a, c);
-        break;
-      case `-`:
-        answer = subtract(a, c);
-        break;
-      case `*`:
-        answer = multiply(a, c);
-        break;
-      case `/`:
-        answer = divide(a, c);
-        break;
-      default:
-        answer = `ERROR`
-    }
-    display.textContent = answer;
-  }
+function operate(operator, a, b) {
+  a = parseFloat(a);
+  b = parseFloat(b);
+  switch (operator) {
+    case `+`:
+      total = add(a, b);
+      break;
+    case `-`:
+      total = subtract(a, b);
+      break;
+    case `x`:
+      total = multiply(a, b);
+      break;
+    case `/`:
+      total = divide(a, b);
+      break;
+    default:
+      total = `ERROR`
+  };
 }
